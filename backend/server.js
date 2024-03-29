9038bb2553ac61aa1f245ad295a7dd72a88b4ec0
Rubel <132301506+rubel2468@users.noreply.github.com>
2024-03-29 23:28:24 +0600

    fourth commit

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
 
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "dashboard", "build", "index.html"));
+});
+
 cloudinary.v2.config({
   cloud_name:"dvozhxu6n",
   api_key:"442261259376517" ,
   api_secret:"Qav6pUTxDhE6IY0Us9DoaY6jBzc" ,
 });
 
-app.listen(`$(window.location.orgin)`, () => {
-
+app.listen(4000, () => {
+console.log("server is running" )
 });
